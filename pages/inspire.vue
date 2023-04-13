2b4d54f45f485aa8f0e39a2f04b2aca198cb4268
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2023-04-13 11:47:32 +0200

    Add Project

---
+++ pages/inspire.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-card></v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage',
+  data(){
+    return{
+      exo: [
+        {title: 'les chocolat', subtitle: 'sont t\'il bon pour vous?', text:'d\'après plusieurs etudes blablabla'},
+        {title: 'les chocolat', subtitle: 'sont t\'il bon pour vous?', text:'d\'après plusieurs etudes blablabla'},
+        {title: 'les chocolat', subtitle: 'sont t\'il bon pour vous?', text:'d\'après plusieurs etudes blablabla'},
+        {title: 'les chocolat', subtitle: 'sont t\'il bon pour vous?', text:'d\'après plusieurs etudes blablabla'}
+      ]
+    }
+  }
+}
+</script>
